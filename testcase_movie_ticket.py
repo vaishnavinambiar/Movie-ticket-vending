ced3f8b929130d69dde107051dd5fde698b2f42f
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-04-09 17:03:46 +0530

    Update testcase_movie_ticket.py

--- testcase_movie_ticket.py
+++ testcase_movie_ticket.py
@@ -1,30 +1,40 @@
 import unittest
-import sys
-from movie4 import *
+from  Movie_ticket_vending import *
 
-class MyTest(unittest.TestCase):
-    def test_one(self):
-        movies = ['Captain Marvel', 'Airaa', 'Lucifer', 'Thadam', 'Super delux','Joseph']
-        self.assertEqual(movies[0], 'Captain Marvel')
+class MovieTicketVending(unittest.TestCase):
 
-    def test_two(self):
-        movies_in_keerthi = ['Captain Marvel', 'Airaa', 'Lucifer']
-        self.assertTrue(True)
+    def test_user_option_one(self):
+        user_option = 'Captain Marvel'
+        self.assertEqual(theatre_showtime_movie[user_option]['theatre'],'mudra')
 
-    def test_three(self):
-        movies_in_mudra = ['Thadam', 'Super delux','Joseph']
-        self.assertNotEqual(movies_in_mudra[0], 'Captain Marvel')
+    def test_user_option_two(self):
+        user_option = 'Lucifer'
+        self.assertEqual(theatre_showtime_movie[user_option]['time'],'6.00PM')
 
-    def test_four(self):
-        theatres_and_seats =  { 'keerthi' : 50, 'mudra' : 25}
-        self.assertEqual(theatres_and_seats.get('keerthi'), 50)
+    def test_user_option_three(self):
+        user_option = 'Thadam'
+        self.assertNotEqual(theatre_showtime_movie[user_option]['seats'], 50)
 
-    def test_five(self):
-        leaftoverseats_mudra = (theatre_and_seats.get('mudra')) -1
-        self.assertNotEqual(leaftoverseats_mudra, 24)
-        
-        
+    def test_user_wish_one(self):
+        seat_type = 'I'
+        self.assertEqual(ticket_type[seat_type], 'Rs 250')
 
+    def test_user_wish_two(self):
+        seat_type = 'II'
+        self.assertEqual(ticket_type[seat_type], 'Rs 150')
 
+    def test_user_wish_three(self):
+        seat_type = 'III'
+        self.assertNotEqual(ticket_type[seat_type], 'Rs 75')
+
+    def test_leftover_seats_one(self):
+        leaftover_seats = theatre_showtime_movie[user_option]['seats'] -1
+        self.assertNotEqual(theatre_showtime_movie[user_option]['seats'] -1, 49)
+
+    def test_leftover_seats_two(self):
+        leaftover_seats = theatre_showtime_movie[user_option]['seats'] -1
+        self.assertEqual(theatre_showtime_movie[user_option]['seats'] -1, 24)
+
+    
 if __name__== '__main__':
     unittest.main()
