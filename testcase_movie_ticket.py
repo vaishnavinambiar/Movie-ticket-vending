d086ae5e58c81e9f35637ce0f26308d2a35ea7f7
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-04-25 14:59:30 +0530

    Update testcase_movie_ticket.py

--- testcase_movie_ticket.py
+++ testcase_movie_ticket.py
@@ -3,17 +3,17 @@ from Movie_ticket_vending import *
 
 class MovieTicketVending(unittest.TestCase):
 
-    def test_user_option_one(self):
-        user_option = 'Captain Marvel'
-        self.assertEqual(theatre_showtime_movie[user_option]['theatre'],'mudra')
+    def test_user_menu_theatres(self):
+         user_option = 'Theatres'
+         self.assertEqual(theatres,["mudra", "keerthi"])
 
-    def test_user_option_two(self):
-        user_option = 'Lucifer'
-        self.assertEqual(theatre_showtime_movie[user_option]['time'],'6.00PM')
+    def test_user_menu_movies(self):
+         user_option = 'Theatres'
+         self.assertEqual(movies, ["Captain Marvel", "Airaa", "Lucifer", "Thadam", "Super delux", "Joseph"])
 
-    def test_user_option_three(self):
-        user_option = 'Thadam'
-        self.assertNotEqual(theatre_showtime_movie[user_option]['seats'], 50)
+    def test_user_menu_showtime(self):
+         user_option = 'Theatres'
+         self.assertEqual(showtimings, ["9.30AM", "1.30PM", "6.00PM"])
 
     def test_user_wish_one(self):
         seat_type = 'I'
@@ -21,36 +21,25 @@ class MovieTicketVending(unittest.TestCase):
 
     def test_user_wish_two(self):
         seat_type = 'II'
-        self.assertEqual(ticket_type[seat_type], 'Rs 150')
+        self.assertFalse(ticket_type[seat_type], 'Rs 150')
 
     def test_user_wish_three(self):
         seat_type = 'III'
-        self.assertNotEqual(ticket_type[seat_type], 'Rs 75')
+        self.assertEqual(ticket_type[seat_type], 'Rs 75')
 
-    def test_leftover_seats_one(self):
-        leaftover_seats = theatre_showtime_movie[user_option]['seats'] -1
-        self.assertNotEqual(theatre_showtime_movie[user_option]['seats'] -1, 49)
+    def test_cardpayment(self):
+        payment_options = 'Card payment'
+        card_number = 4563456987556
+        cvv = 985
+        self.assertNotEqual((card_number,cvv), (4563456987556,985))
 
-    def test_leftover_seats_two(self):
-        leaftover_seats = theatre_showtime_movie[user_option]['seats'] -1
-        self.assertEqual(theatre_showtime_movie[user_option]['seats'] -1, 24)
-
-    def test_one_movies_in_particular_theatre(self):
-        user_need_theatre = 'mudra'
-        self.assertEqual(movies_in_particular_theatre[user_need_theatre]['movies'], 'Captain Marvel Airaa Lucifer')
-
-    def test_two_movies_in_particular_theatre(self):
-        user_need_theatre = 'keerthi'
-        self.assertNotEqual(movies_in_particular_theatre[user_need_theatre]['movies'], 'Thadam  Super delux Joseph')
+    def test_netbanking(self):
+        payment_options = 'Netbanking'
+        customer_id = 445522663
+        transaction_password= 'vaishnavi1996'
+        osp_number = 852369
+        self.assertTrue((customer_id,transaction_password,osp_number), (445522663,'vaishnavi1996',852369))
         
-    def test_one_theatre_showtime(self):
-        user_choice = 'Lucifer'
-        self.assertTrue(('mudra', '6.00PM'), True)
-
-    def test_two_theatre_showtime(self):
-        user_choice = 'Captain Marvel'
-        self.assertFalse(('mudra', '9.30AM'), False)
-
-
+        
 if __name__== '__main__':
     unittest.main()
