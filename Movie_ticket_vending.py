9159b0335f3f815ee4c2b16d387c850d452ed0ba
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-04-25 09:55:30 +0530

    Update Movie_ticket_vending.py

--- Movie_ticket_vending.py
+++ Movie_ticket_vending.py
@@ -2,87 +2,127 @@
 import sys
 import csv
 
-movies = ['Captain Marvel', 'Airaa', 'Lucifer', 'Thadam', 'Super delux','Joseph']
-movies_in_particular_theatre = {'mudra':{'movies': 'Captain Marvel Airaa Lucifer'},
-                                'keerthi':{'movies': 'Thadam  Super delux Joseph'}}
-theatre_showtime_movie = {'Captain Marvel':{'theatre':'mudra', 'seats': 50, 'time':'9.30AM'},
-                          'Airaa':{'theatre':'mudra', 'seats': 50, 'time':'1.30PM'},
-                          'Lucifer':{'theatre':'mudra', 'seats': 50, 'time':'6.00PM'},
-                          'Thadam':{'theatre':'keerthi', 'seats': 25, 'time':'9.30AM'},
-                          'Joseph':{'theatre':'keerthi', 'seats': 25, 'time':'1.30PM'},
-                          'Super delux':{'theatre':'keerthi', 'seats': 25, 'time':'6.00PM'}}
+theatres = ["mudra", "keerthi"]
+showtimings = ["9.30AM", "1.30PM", "6.00PM"]
+movies = ["Captain Marvel", "Airaa", "Lucifer", "Thadam", "Super delux", "Joseph"]
+movies_in_particular_theatre = {"mudra":{"movies": "Captain-Marvel  Airaa  Lucifer", "time": "9.30AM  1.30PM  6.00PM"},
+                                "keerthi":{"movies": "Thadam  Super-Delux  Joseph", "time": "9.30AM  1.30PM  6.00PM"}}
+theatre_showtime_seats = {"Captain Marvel":{"theatre":"mudra", "seats": 50, "time":"9.30AM"},
+                          "Airaa":{"theatre":"mudra", "seats": 50, "time":"1.30PM"},
+                          "Lucifer":{"theatre":"mudra", "seats": 50, "time":"6.00PM"},
+                          "Thadam":{"theatre":"keerthi", "seats": 25, "time":"9.30AM"},
+                          "Joseph":{"theatre":"keerthi", "seats": 25, "time":"1.30PM"},
+                          "Super delux":{"theatre":"keerthi", "seats": 25, "time":"6.00PM"}}
+movie_and_theatre = {"9.30AM":{"theatre1":"mudra:", "movie1":"Captain Marvel", "theatre2":"keerthi:", "movie2":"Thadam"},
+                     "1.30PM":{"theatre1":"mudra:", "movie1":"Airaa", "theatre2":"keerthi:", "movie2":"Joseph"},
+                     "6.00PM": {"theatre1":"mudra:", "movie1":"Lucifer", "theatre2":"keerthi:", "movie2":"Super delux"}}
 balance_seats_after_booking = []
-ticket_type= {'I': 'Rs 250', 'II': 'Rs 150', 'III': 'Rs 75'}
+ticket_type= {"I": "Rs 250", "II": "Rs 150", "III": "Rs 75"}
 
-user_option= raw_input("select a movie: ")
 
-def getDetails(user_option):
-    
-    """Getting details of theatre, show time and seat available when user selects a movie"""
-    return "theatre, seats and show time are:",theatre_showtime_movie[user_option]['theatre'],theatre_showtime_movie[user_option]['seats'],theatre_showtime_movie[user_option]['time']
+user_option = int(raw_input("Select any from Menu\n1.Theatres\n2.Movies\n3.Showtime "))
+
+def getDetails():
+    """Getting details of theatre, show time and seat available when user select from the menu"""
+    if user_option == 1:
+        return theatres
+    elif user_option == 2:
+        return movies
+    elif user_option == 3:
+        return showtimings
+    else:
+        return "Invalid"
+
+
+def movies_theatre_and_showtime():
+    """Fetching movies, theatres and showtime according to selected menu"""
+    if user_option ==1:
+        select_theatre=select_theatre = raw_input("Select a theatre: ")
+        return movies_in_particular_theatre[select_theatre]["movies"],movies_in_particular_theatre[select_theatre]["time"]
+    elif user_option ==2:
+        select_movie = raw_input("Select a movie: ")
+        return theatre_showtime_seats[select_movie]["theatre"],theatre_showtime_seats[select_movie]["time"]
+    elif user_option == 3:
+        select_showtime = raw_input("Select a showtime: ")
+        print movie_and_theatre[select_showtime]["theatre1"], movie_and_theatre[select_showtime]["movie1"], movie_and_theatre[select_showtime]["theatre2"], movie_and_theatre[select_showtime]["movie2"]
+        select_movie = raw_input("Select a movie you want ")
+        return "Selected"
+    else:
+        return "Invalid"
 
 
 def getSeattype():
     """Getting the seat type and price accordingly"""
     user_wish = raw_input("Do you want to book ticket for this movie? Select Y or N ")
-    if user_wish == str('y'):
+    if user_wish == str("y"):
         seat_type = raw_input("Select seat type I II or III ")
-        return "Ticket price: ",ticket_type[seat_type]
+        return ("Ticket price is ",ticket_type[seat_type],"and successfully booked")
+    else:
+        return "Invalid, Sorry try again!"
+    sys.exit()
+        
 
 
 def getBooked():
     """Booking the seats"""
-    booking = raw_input("To continue booking press Y ")
-    if booking == str('y'):
-        leftover_seats = theatre_showtime_movie[user_option]['seats'] -1
-        balance_seats_after_booking.append(leftover_seats)
-        return "left seats are: ",balance_seats_after_booking
+    if user_option == 1:
+        fix_theatre = raw_input("Fix this theatre? Y or N ")
+        if fix_theatre == str("y"):
+            select_one_movie = raw_input("Which movie do you want in this theatre? ")
+            print getSeattype()
+        else:
+            return "Invalid"
 
+    elif user_option == 2:
+        fix_movie = raw_input("Fix this movie? Y or N ")
+        if fix_movie == str("y"):
+            print getSeattype()
+        else:
+            return "Invalid"
+
+    elif user_option == 3:
+        fix_showtime = raw_input("Do you want to book ticket? Y or N ")
+        if fix_showtime == str("y"):
+            print getSeattype()
+        else:
+            print "Invalid"
+            
+            
 
 def getPayed():
     """Payment making through netbanking or card"""
     payment = raw_input("To continue to payment press Y ")
-    if payment == str('y'):
-        payment_options = raw_input("1. Card payment\n 2. Netbanking ")
-        if payment_options == str('1'):
-            raw_input("Enter card no & cvv")
+    if payment == str("y"):
+        payment_options = raw_input("1.Card payment\n2.Netbanking ")
+        if payment_options == str("1"):
+            int(input("Enter card no: "))
+            int(input("Enter cvv: "))
             return "Yay! payment successfull"
-        elif payment_options == str('2'):
-            raw_input ("Enter Customer ID & password")
+        elif payment_options == str("2"):
+            int(input("Enter Customer ID: "))
+            raw_input("Enter ur transpassword: ")
+            int(input("Enter the OSP number you recieved in registered mobile number: "))
             return "Yay! payment successfull"
+    else:
+        return "Invalid"
 
-def rebooking():
-  """To rebook or not"""
-    rebook = raw_input("Do you want to rebook Y or N? ")
-    if rebook == str('y'):
+
+
+def getContinued():
+    """To get continue booking"""
+    continue_booking = raw_input("Do you want to rebook Y or N? ")
+    if continue_booking == str("y"):
+        print user_option
+        print getDetails()
+        print movies_theatre_and_showtime()
         print getSeattype()
-        leftover_seats = balance_seats_after_booking[0] -1
-        balance_seats_after_booking.append(leftover_seats)
-        return leftover_seats 
         print getPayed()
-        
-    elif rebook == str('n'):
+    elif continue_booking == str("n"):
         return "Thankyou! Enjoy your movies"
-        sys.exit()
-        
-        
-def getAllmovies():
-  """ To list out all movies"""
-    raw_input("To see all trending movies press Enter")
-    return movies
+    else:
+        return "Invalid"
 
-  
-def Movies_particular_theatre():
-  """To see movies in particular theatre"""
-    user_need_theatre = raw_input("Which theatre movies you want to see?")
-    return movies_in_particular_theatre[user_need_theatre]['movies']   
- 
 
-def getTheatre_showtime():
-    """ To list theater with show, given a movie name"""
-    user_choice = raw_input("Give a movie name ")
-    return theatre_showtime_movie[user_choice]['theatre'], theatre_showtime_movie[user_choice]['time']
-                               
 
 with open('Movie_vending_machine.csv', mode='w') as moviename_file:        #writing  csv file
     moviename_writer = csv.writer(moviename_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -93,16 +133,14 @@ with open('Movie_vending_machine.csv', mode='w') as moviename_file:        #writ
     moviename_writer.writerow(['Mudra', '6.00PM', 'Lucifer'])
     moviename_writer.writerow(['Keerthi', '9.30AM', 'Thadam'])
     moviename_writer.writerow(['Keerthi', '1.30PM', 'Josseph'])
-    moviename_writer.writerow(['Keerthi', '6.00PM', 'Super Delux'])   
+    moviename_writer.writerow(['Keerthi', '6.00PM', 'Super Delux'])         
+            
+            
 
 
-
-if __name__ == "__main__":                   #main program
-    print getDetails(user_option)
-    print getSeattype()
+if __name__ == "__main__":
+    print getDetails()
+    print movies_theatre_and_showtime()
     print getBooked()
     print getPayed()
-    print rebooking()
-    print getAllmovies()
-    print Movies_particular_theatre()
-    print getTheatre_showtime()
+    print getContinued()
